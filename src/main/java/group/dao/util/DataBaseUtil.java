3884c1158c14a14be6fa2319fd883ef5f2a93139
SuRuixian <741617293@qq.com>
2022-12-30 18:46:22 +0800

    优化数据库连接

--- src/main/java/group/dao/util/DataBaseUtil.java
+++ src/main/java/group/dao/util/DataBaseUtil.java
@@ -4,25 +4,42 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 
 public class DataBaseUtil {
 
     // 饿汉单例,提高加载速度
 
-    private static final MongoDatabase mongoDatabase;
-    private static final MongoClient mongoClient;
+    private static MongoDatabase mongoDatabase;
+    private static MongoClient mongoClient;
     private DataBaseUtil(){}
 
     static{
-        ConnectionString connectionString = new ConnectionString("mongodb://1.15.118.125:27017");
-        //ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
-        mongoClient = MongoClients.create(settings);
-        mongoDatabase = mongoClient.getDatabase("NIC");
+        try {
+            ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .build();
+            mongoClient = MongoClients.create(settings);
+            mongoDatabase = mongoClient.getDatabase("NIC");
+            Document test = mongoDatabase.getCollection("User")
+                    .find()
+                    .first();
+            if (test == null) {
+                throw new RuntimeException();
+            }
+        } catch (Exception e) {
+            System.out.println("change");
+            ConnectionString connectionString = new ConnectionString("mongodb://1.15.118.125:27017");
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .build();
+            mongoClient = MongoClients.create(settings);
+            mongoDatabase = mongoClient.getDatabase("NIC");
+        }
     }
 
     public static MongoDatabase getMongoDB(){
