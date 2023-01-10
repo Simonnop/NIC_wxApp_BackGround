532f89e19f5956bd5b065dda0dc1f78fc3da80ea
SuRuixian <741617293@qq.com>
2023-01-10 21:28:54 +0800

    新增socket连接

---
+++ src/main/java/group/controller/LessonServlet.java
@@ -0,0 +1,96 @@
+package group.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import group.controller.util.LogPrinter;
+import group.exception.AppRuntimeException;
+import group.exception.ExceptionKind;
+import group.service.UserService;
+import group.service.helper.SocketHelper;
+import group.service.helper.UserHelper;
+import group.service.impl.UserServiceImpl;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URLDecoder;
+
+public class LessonServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(LoginServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            String method = req.getParameter("method");
+            switch (method) {
+                case "get":
+                    getLessonResponse(req, resp);
+                    break;
+                default:
+                    throw new AppRuntimeException(ExceptionKind.REQUEST_INFO_ERROR);
+            }
+        } catch (Exception e) {
+            Writer out = resp.getWriter();
+            JSONObject result = new JSONObject();
+
+            if (e instanceof AppRuntimeException) {
+                result.put("code", ((AppRuntimeException) e).getCode());
+                result.put("msg", ((AppRuntimeException) e).getMsg());
+            } else {
+                result.put("code", 98);
+                result.put("msg", "后端LessonServlet处理错误");
+                logger.error(URLDecoder.decode(req.getQueryString(), "utf-8"));
+                LogPrinter.printException(logger, e);
+            }
+
+            String resultStr = result.toJSONString();
+            out.write(resultStr);
+            out.flush();
+            System.out.println(resultStr);
+        }
+    }
+
+    protected void getLessonResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        Writer out = resp.getWriter();
+        JSONObject result = new JSONObject();
+
+        String data = req.getParameter("data");
+        JSONObject dataJson = JSONObject.parseObject(data);
+
+        String userid = (String) dataJson.get("userid");
+        String password = (String) dataJson.get("password");
+        if (userid == null && password == null) {
+            throw new AppRuntimeException(ExceptionKind.REQUEST_INFO_ERROR);
+        }
+
+        String lesson = null;
+        for (; ; ) {
+            if (lesson == null) {
+                lesson = SocketHelper.getSocketHelper().getUserLesson(userid, password);
+            } else {
+                break;
+            }
+        }
+
+        result.put("code", 102);
+        result.put("msg", "课表爬取成功");
+        result.put("data", lesson);
+
+        String resultStr = result.toJSONString();
+        out.write(resultStr);
+        out.flush();
+        System.out.println(resultStr);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
--- src/main/java/group/service/helper/SocketHelper.java
+++ src/main/java/group/service/helper/SocketHelper.java
@@ -3,14 +3,22 @@ package group.service.helper;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 public class SocketHelper {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final SocketHelper socketHelper = new SocketHelper();
+    private SocketHelper() {
+    }
+    public static SocketHelper getSocketHelper() {
+        return socketHelper;
+    }
 
+    public String getUserLesson(String userid, String password) {
         String ip = "localhost";        // 设置发送地址和端口号
         int port = 12346;
         Socket socket = null;
+        String result = null;
         try {
             // 连接服务器
             socket = new Socket(ip, port);
@@ -19,10 +27,7 @@ public class SocketHelper {
             OutputStreamWriter opsw = new OutputStreamWriter(ops);
             BufferedWriter bw = new BufferedWriter(opsw);
 
-            String userid = "U208911390";
-            String pwd = "123456";
-
-            String stuInfo =  "{\"userid\":\""+ userid +"\",\"pwd\":\""+ pwd +"\"}";
+            String stuInfo = "{\"userid\":\"" + userid + "\",\"pwd\":\"" + password + "\"}";
 
             bw.write(stuInfo);
             bw.flush();
@@ -31,16 +36,17 @@ public class SocketHelper {
             InputStream in = socket.getInputStream();   //读取数据
 
             // 包装输入流，输出流，包装一下可以直接传输字符串，不包装的话直接使用InputStream和OutputStream只能直接传输byte[]类型数据
-            BufferedReader inRead = new BufferedReader(new InputStreamReader(in));
+            BufferedReader inRead = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
 //			PrintWriter outWriter = new PrintWriter(out);
 
             // 接受应答
-            String result = inRead.readLine();  // 使用了包装后的输入流方便读取消息
+            result = inRead.readLine();
             System.out.println(result);
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
+        return result;
+    }
 }
